32e8ae8d50f8a487dfbd3f6175ae2e508c14cc65
Robert <regbit4@gmail.com>
2020-11-09 20:50:08 +0300

    0 division fix

--- task_3_4.py
+++ task_3_4.py
@@ -1,6 +1,6 @@
 def my_func(x, y):
 	"""my_func(x, y) -> float"""
-	if (type(x) is not int and type(x) is not float) or x < 0:
+	if (type(x) is not int and type(x) is not float) or x <= 0:
 		return f"'x' must be positive integer or float! x = {x, type(x)}"
 
 	if type(y) is not int or y >= 0:
